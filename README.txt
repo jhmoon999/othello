8aaf17a41fc487b1d67d884c84f02c7d979be797
Katerina <koeka@me.com>
2018-03-12 23:39:18 -0700

    Wrote deranged not quite minimax

---
+++ README.txt
@@ -0,0 +1,8 @@
+Our team is Jason and Katerina. 
+Jason set up the repository on his github, wrote the random player that works. 
+We work together on the heuristics regarding the score and edge positions, with
+KAterina writing the initial messy code, Jason rewriting it in a smart way,
+and KAterina debugging. 
+We both ran test games many many times and amde sure there are no bugs.
+Katerina wrote a messy version of minimax that is not really minimax because we
+assume the opponent only asssesses the current board state. It wins on average 7 out of 10 games.
--- player.cpp
+++ player.cpp
@@ -53,7 +53,10 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
 
     Move *m = new Move(0, 0), *best_move = new Move(0, 0);
     int move_points, best_move_points = -1000;
+    int opm_points, worst_move_points = 1000;
     Board *b_copy = b->copy();
+    Board *b2_copy = b->copy();
+    int free_cells = 0;
 
     if (b->hasMoves(playerside)) {
         for (int i = 0; i < 8; i++) {
@@ -65,28 +68,108 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
                 if (b->checkMove(m, playerside)) {
                     //cerr << m->getX() << " " << m->getY() << endl;
 
+                    //we check that m is a valid move on our side
+                    //now for m we should check what is the min score we get after 
+                    //the opponent makes a move
+
+
+
                     b_copy = b->copy();
                     b_copy->doMove(m, playerside);
 
-                    move_points = b_copy->count(playerside) - b_copy->count(otherside);
+                    //b_copy is a mew board with our move
+                    //now we try all the moves our guy can make 
 
-                    if ((i % 7 == 0) && (j % 7 == 0)) {
-                        move_points = move_points * 4;
+                    //if the guy doesn't have moves our score is just our score
+                    // im too lazy to write a separate func for that
+
+                    free_cells = 64 - b_copy->countBlack() -  b_copy->countWhite();
+
+                    if ((! b_copy->hasMoves(otherside)) || (free_cells < 4) || (b_copy->count(otherside) > 2* b_copy->count(playerside)))
+
+                    {
+
+                        move_points = b_copy->count(playerside) - b_copy->count(otherside);
+
+
                     }
 
-                    else if ((i % 5 == 1) && (j % 5 == 1))
-                        move_points *= -4;
+                    else //if the guy has moves
+
+                        //we check what's our worst score after his move
+                        //that's the score for our move
+
+                    {
+
+                        Move *opm = new Move(0, 0); //opponent move
+                        for (int k = 0; k< 8; k++)
+                        {
+                            for (int n = 0; n < 8; n++)
+                            {
+                                opm->setX(k);
+                                opm->setY(n);
+
+                                if (b_copy->checkMove(opm, otherside)) //if valid move for the guy
+                                {
+
+                                    b2_copy = b_copy->copy();
+                                    b2_copy->doMove(opm, otherside); //we made a copy of the b_copy and applied oppoent move
+
+
+                                    //the guy makes a move so that our score after his is minimal
+                                    //so he does the same stuff copies the board and checks for every move our score
+                                    //haven't implemented yet
+
+                                    opm_points = b2_copy->count(playerside) - b2_copy->count(otherside);
+
+
+                                    if ((n % 7 == 0) || (k % 7 == 0)) 
+                                        opm_points += abs(opm_points) * (-3);
+
+
+
+
+                                    //we counted our points after the guys move, thats opm_points
+                                    //he will make a move so that we have less points
+
+                                    if (opm_points < worst_move_points)
+                                    {
+                                        worst_move_points = opm_points;
+                                    }
+
+                                }
+
+                            }
+                        }
+
+
+                        move_points = worst_move_points;
+                    }
                     
-                    else if ((i % 5 == 1) && (j % 7 == 0))
-                        move_points *= -3;
 
-                    else if ((j % 5 == 1) && (i % 7 == 0))
-                        move_points *= -3;
+                    if ((free_cells > 4) && (b_copy->count(playerside) > 4))
+                    {
 
-                    else if ((i % 7 == 0) || (j % 7 == 0))
-                        move_points *= 3;
+                        if ((i % 7 == 0) && (j % 7 == 0)) 
+                            move_points += abs(move_points) * 5;
+                        
+
+                        else if ((i % 5 == 1) && (j % 5 == 1))
+                            move_points += abs(move_points) * (-3);
+                            
+                        else if ((i % 5 == 1) && (j % 7 == 0))
+                            move_points += abs(move_points) * (-2);
+
+                        else if ((j % 5 == 1) && (i % 7 == 0))
+                            move_points += abs(move_points) *(-2);
+
+                        else if ((i % 7 == 0) || (j % 7 == 0))
+                            move_points += abs(move_points)* (5);
+                    }
 
 
+
+                    //old code
                     if (best_move_points < move_points) {
                         best_move_points = move_points;
                         best_move->setX(i);
